80c76db32d33fd8ffeff0dd3b579881a8231eac1
SachinVedGupta <sachinvgupta9@gmail.com>
2025-02-06 02:25:48 -0500

    final algorithm printing out more steps than it should

--- src/main/java/ca/mcmaster/se2aa4/mazerunner/Navigate.java
+++ src/main/java/ca/mcmaster/se2aa4/mazerunner/Navigate.java
@@ -60,7 +60,9 @@ class Navigate extends Maze {
       }
     }
 
-    if (!(rowIndex < height && colIndex < width)) { return output; } // not currently on a valid square
+    if (rowIndex >= height || colIndex >= width || rowIndex < 0 || colIndex < 0) { return output; } // {-1, -1, -1} not currently inside the board --> if output[2] == -1 that its invalid
+    if (maze[rowIndex][colIndex] == 1) { return output; } // {-1, -1, -1} not currently on a valid square (its a wall)
+
     output[0] = rowIndex;
     output[1] = colIndex;
 
@@ -81,13 +83,95 @@ class Navigate extends Maze {
     }
     output[2] = direction;
 
+    // output:
+        // new row index
+        // new col index
+        // new direction facing:
+          // N = 1, E = 2, S = 3, W = 4
+
+    // System.out.printf(" %c ", step);
     return output;
   }
 
+  char numToDir(int numDir){
+    switch (numDir) {
+      case 1:
+        return('N');
+      case 3:
+        return('S');
+      case 4:
+        return('W');
+      case 2:
+        return ('E');
+    }
+
+    return 'Z';
+  }
+
+  boolean isRight(int rowIndex, int colIndex, char curr_direction) {
+    int[] one = verifyOneStep(rowIndex, colIndex, curr_direction, 'R');
+    curr_direction = numToDir(one[2]);
+
+    int[] two = verifyOneStep(rowIndex, colIndex, curr_direction, 'F');
+
+    if (two[2] == -1) {
+      return true;
+    }
+    return false;
+  }
+
+  boolean canFor(int rowIndex, int colIndex, char curr_direction) {
+    int[] one = verifyOneStep(rowIndex, colIndex, curr_direction, 'F');
+
+    if (one[2] == -1) {
+      return false;
+    }
+    return true;
+  }
+
+
   boolean printAlgorithm() {
     // MVP Algorithm try going forward
-    System.out.println("Try Path:");
+    System.out.println("\n\nTry Path: \n\n");
 
+    int rowIdx = startRow;
+    int colIdx = 0;
+    char direction = 'E';
+    
+    while (colIdx != width - 1) {
+      int[] ans;
+      System.out.println(isRight(rowIdx, colIdx, direction));
+      if (isRight(rowIdx, colIdx, direction) && canFor(rowIdx, colIdx, direction)) {
+        // move forward
+        ans = verifyOneStep(rowIdx, colIdx, direction, 'F');
+        System.out.printf(" %c ", 'F');
+      }
+      else if (isRight(rowIdx, colIdx, direction)) {
+        // turn left
+        ans = verifyOneStep(rowIdx, colIdx, direction, 'L');
+        System.out.printf(" %c ", 'L');
+      }
+      else { // in the open, so turn right and move forward
+        // right
+        // forward
+        int[] ans1 = verifyOneStep(rowIdx, colIdx, direction, 'R');
+        direction = numToDir(ans1[2]);
+
+        ans = verifyOneStep(rowIdx, colIdx, direction, 'F');
+      }
+
+      rowIdx = ans[0];
+      colIdx = ans[1];
+      direction = numToDir(ans[2]);
+    }
+
+
+    System.out.println("\n\nPath is done\n\n");
+    return true;
+
+    //row = startRow
+
+    /*
     char currentDirection = 'F';
     for (int col = 0; col < width; col++) {
       if (maze[startRow][col] == 0) {
@@ -99,5 +183,6 @@ class Navigate extends Maze {
       }
     }
     return true;
+    */
   }
 }
